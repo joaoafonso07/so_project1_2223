998d924dbac7b59d0517ad0555ff96e2a8da5187
Deadmastic <diogo.ls.vieira@gmail.com>
2022-12-18 18:13:19 +0000

    Introdução ao teste thread
    temos de ver isto melhor

---
+++ tests/thread01.c
@@ -0,0 +1,26 @@
+#include "fs/operations.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+
+
+
+
+int main(){
+	pthread_t tid[3];
+
+	/*TODO*/
+	/*Preciso de ajuda aqui....*/
+	/*
+	if (pthread_create(&tid[0], NULL, ,) != 0) {
+	}
+
+
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+	pthread_join(tid[2, NULL])
+	*/
+
+}
